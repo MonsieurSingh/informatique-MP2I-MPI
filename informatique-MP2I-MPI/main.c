9cf325a8e0f3436c1000ec286a251eb5d2c8452a
MonsieurSingh <teghjyot@icloud.com>
2025-11-18 22:02:28 +0100

    Initial Commit

---
+++ informatique-MP2I-MPI/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  informatique-MP2I-MPI
+//
+//  Created by Teghjyot Singh on 18/11/2025.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return EXIT_SUCCESS;
+}
