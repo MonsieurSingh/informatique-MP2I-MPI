5f823ec4b2e0bb33730be4195153f40788e1b521
MonsieurSingh <teghjyot@icloud.com>
2025-11-21 22:31:00 +0100

    add: probability distribution functions

---
+++ algorithms/randomised/probability/probability.c
@@ -0,0 +1,59 @@
+//
+//  probability.c
+//  probability
+//
+//  Created by Teghjyot Singh on 20/11/2025.
+//
+
+#include "probability.h"
+
+void	init_rand(void)
+{
+	srand((unsigned int)time(NULL));
+}
+
+float	r(void)
+{
+	return ((float)rand() / (float)RAND_MAX);
+}
+
+int	bernouilli(float p)
+{
+	if (r() < p)
+		return (true);
+	else
+		return (false);
+}
+
+int	geometrique(float p)
+{
+	return ((int)floor(log(1 - r()) / log(1 - p)) + 1);
+}
+
+int	binomiale(int n, float p)
+{
+	int	i;
+	int	k;
+
+	k = 0;
+	i = 0;
+	while (i < n)
+	{
+		if (bernouilli(p))
+			k++;
+		i++;
+	}
+	return (k);
+}
+
+int main(int argc, const char * argv[])
+{
+	size_t	i;
+	size_t	count;
+
+	(void)argc;
+	(void)argv;
+	init_rand();
+	printf("Measured probability: %d\n", binomiale(500000, 0.5));
+	return EXIT_SUCCESS;
+}
---
+++ algorithms/randomised/probability/probability.h
@@ -0,0 +1,19 @@
+//
+//  probability.h
+//  probability
+//
+//  Created by Teghjyot Singh on 20/11/2025.
+//
+
+#ifndef probability_h
+#define probability_h
+
+#include <math.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+float	r(void);
+int		bernouilli(float p);
+#endif /* probability_h */
